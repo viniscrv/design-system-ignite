96d54ef7eccb95e1b299588dca68bbbd8a7b95a6
Vinícius <viniscrv@gmail.com>
2022-12-21 15:25:51 -0300

    configurado storybook

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,3 +1,30 @@
-export function App() {
-  return <h1>Hello World</h1>
-}
+import { styled } from "./styles";
+import { ComponentProps } from "react";
+
+export type ButtonProps = ComponentProps<typeof Button>
+
+export const Button = styled("button", {
+  fontFamily: "$default",
+  backgroundColor: "$ignite300",
+  borderRadius: "$sm",
+  border: 0,
+  fontWeight: "bold",
+  color: "$white",
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: "$2 $4",
+      },
+      big: {
+        fontSize: 16,
+        padding: "$3 $6",
+      }
+    } 
+  },
+  
+  defaultVariants: {
+    size: "small",
+  }
+});
